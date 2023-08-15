9f0dfba7c5ec26c6928b3e044b2bcb7854587f1d
usc-KWM007 <KWM007@student.usc.edu.au>
2023-08-15 17:50:10 +1000

    Chapter 8 Fragments implemented

---
+++ app/src/main/java/com/android/criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.android.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean)
