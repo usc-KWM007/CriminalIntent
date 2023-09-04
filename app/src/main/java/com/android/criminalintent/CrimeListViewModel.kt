8777c7cb36f3567aefdec7e02e91befe273367f4
usc-KWM007 <KWM007@student.usc.edu.au>
2023-09-04 16:58:39 +1000

    Added tutorial chapter 13 workshop fix

--- app/src/main/java/com/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/android/criminalintent/CrimeListViewModel.kt
@@ -1,15 +1,11 @@
 package com.android.criminalintent
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.util.Date
-import java.util.UUID
 
 
 private const val TAG = "CrimeListViewModel"
@@ -18,8 +14,7 @@ class CrimeListViewModel: ViewModel() {
     private val crimeRepository = CrimeRepository.get()
 
     private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
-    val crimes: StateFlow<List<Crime>>
-        get() = _crimes.asStateFlow()
+    val crimes: StateFlow<List<Crime>> get() = _crimes.asStateFlow()
     init {
         viewModelScope.launch {
             crimeRepository.getCrimes().collect {
